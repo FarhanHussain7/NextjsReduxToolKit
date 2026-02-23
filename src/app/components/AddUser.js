f3924f8352f7795268e3da12bc15290fb35beff2
Farhanhussain7 <farhanhussain81303@gmail.com>
2026-02-23 23:38:53 +0530

    First commit

---
+++ src/app/components/AddUser.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+export default function AddUser() {
+  return (
+    <div>
+      <h1>Add User</h1>
+        <p>This is the Add User page.</p>
+        <input type="text" placeholder="Enter user name" />
+        <button>Add User</button>
+    </div>
+  );
+}   
---
+++ src/app/components/DisplayUser.js
@@ -0,0 +1,10 @@
+import "./style.css";
+export default function DisplayUser() {
+  return (
+    <div>
+      <h1>Display User</h1>
+        <p>This is the Display User page.</p>
+
+    </div>
+  );
+}   
