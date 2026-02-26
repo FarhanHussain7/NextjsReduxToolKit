3c55cf2f0802f6ef649433a791157bcf3bad169e
Farhanhussain7 <farhanhussain81303@gmail.com>
2026-02-27 01:08:53 +0530

    Local Storage System

--- src/app/redux/slice.js
+++ src/app/redux/slice.js
@@ -1,8 +1,8 @@
 // A slice is a collection of Redux reducer logic and actions for a single feature in your app, typically defined together in a single file. The createSlice() function accepts an initial state, an object full of reducer functions, and a "slice name", and automatically generates action creators and action types that correspond to the reducers and state.
 import { createSlice, nanoid } from "@reduxjs/toolkit";
-
+// import { configureStore } from "@reduxjs/toolkit";
 const initialState = {
-    users: [],
+    users: JSON.parse(localStorage.getItem("users")) || [],
 };
 
 const userSlice = createSlice({
@@ -16,6 +16,8 @@ const userSlice = createSlice({
                     name: action.payload.name,
                 };
                     state.users.push(data);
+                    let userData = JSON.stringify(state.users);
+                    localStorage.setItem("users", userData);
                     // console.log("User added to state:", data);
                 }
             },
@@ -24,6 +26,8 @@ const userSlice = createSlice({
                     const userId = action.payload; // no .id here
                     state.users = state.users.filter((user) => user.id !== userId);
                     console.log("User removed from state with ID:", userId);
+                    let userData = JSON.stringify(state.users);
+                    localStorage.setItem("users", userData);
                 }
             }
         },
