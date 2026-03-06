686d53080628fc3e709968397fa0e43c8e6c47d2
Farhanhussain7 <farhanhussain81303@gmail.com>
2026-03-06 08:35:31 +0530

    Local storage delete

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,23 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
